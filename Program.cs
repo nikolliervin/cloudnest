0fcc958a26f09c0e819736d00cd56e7314bcb7e8
nikolliervin <nikolliervin@gmail.com>
2024-12-05 00:13:06 +0100

    Moved Bearer Transformer Class

--- Program.cs
+++ Program.cs
@@ -64,7 +64,6 @@ builder.Services.AddOpenApi("v1", options =>
 {
     options.AddDocumentTransformer<BearerSecuritySchemeTransformer>();
 });
-builder.Services.AddOpenApi("public");
 
 
 var app = builder.Build();
@@ -91,33 +90,3 @@ app.MapControllers();
 
 app.Run();
 
-// internal sealed class BearerSecuritySchemeTransformer(Microsoft.AspNetCore.Authentication.IAuthenticationSchemeProvider authenticationSchemeProvider) : IOpenApiDocumentTransformer
-// {
-//     public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
-//     {
-//         var authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
-//         if (authenticationSchemes.Any(authScheme => authScheme.Name == "Bearer"))
-//         {
-//             var requirements = new Dictionary<string, OpenApiSecurityScheme>
-//             {
-//                 ["Bearer"] = new OpenApiSecurityScheme
-//                 {
-//                     Type = SecuritySchemeType.Http,
-//                     Scheme = "bearer", 
-//                     In = ParameterLocation.Header,
-//                     BearerFormat = "Json Web Token"
-//                 }
-//             };
-//             document.Components ??= new OpenApiComponents();
-//             document.Components.SecuritySchemes = requirements;
-
-//             foreach (var operation in document.Paths.Values.SelectMany(path => path.Operations))
-//             {
-//                 operation.Value.Security.Add(new OpenApiSecurityRequirement
-//                 {
-//                     [new OpenApiSecurityScheme { Reference = new OpenApiReference { Id = "Bearer", Type = ReferenceType.SecurityScheme } }] = Array.Empty<string>()
-//                 });
-//             }
-//         }
-//     }
-// }
